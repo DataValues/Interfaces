2503f7796d63478596d0221ce1e614a0efda0954
Jeroen De Dauw <jeroendedauw@gmail.com>
2013-11-16 04:55:10 +0100

    Update DataValuesInterfaces.php

--- DataValuesInterfaces.php
+++ DataValuesInterfaces.php
@@ -22,22 +22,6 @@ define( 'DATAVALUES_INTERFACES_VERSION', '0.1' );
  */
 define( 'DataValuesInterfaces_VERSION', DATAVALUES_INTERFACES_VERSION );
 
-// If one of the dependencies has not been loaded yet, attempt to include the Composer autoloader.
-if ( !defined( 'DATAVALUES_VERSION' ) && is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
-	include_once( __DIR__ . '/vendor/autoload.php' );
-}
-
-// Attempt to include the DataValues lib if that hasn't been done yet.
-// This is the path the DataValues entry point will be at when loaded as MediaWiki extension.
-if ( !defined( 'DATAVALUES_VERSION' ) && is_readable( __DIR__ . '/../DataValues/DataValues.php' ) ) {
-	include_once( __DIR__ . '/../DataValues/DataValues.php' );
-}
-
-// Only initialize the extension when all dependencies are present.
-if ( !defined( 'DATAVALUES_VERSION' ) ) {
-	throw new Exception( 'You need to have the DataValues library loaded in order to use DataValuesInterfaces' );
-}
-
 spl_autoload_register( function ( $className ) {
 	$className = ltrim( $className, '\\' );
 
