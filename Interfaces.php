136ecf4245343b70b1ad733099aff7f88dd42942
Kunal Mehta <kunalm@legoktm.com>
2017-08-09 11:11:44 -0400

    Remove MediaWiki registration
    MediaWiki shows credits of installed composer libraries, so this isn't necessary.

--- Interfaces.php
+++ Interfaces.php
@@ -16,17 +16,3 @@ if ( defined( 'DATAVALUES_INTERFACES_VERSION' ) ) {
 }
 
 define( 'DATAVALUES_INTERFACES_VERSION', '0.2.3' );
-
-if ( defined( 'MEDIAWIKI' ) ) {
-	$GLOBALS['wgExtensionCredits']['datavalues'][] = [
-		'path' => __DIR__,
-		'name' => 'DataValues Interfaces',
-		'version' => DATAVALUES_INTERFACES_VERSION,
-		'author' => [
-			'[https://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]',
-		],
-		'url' => 'https://github.com/DataValues/Interfaces',
-		'description' => 'Defines interfaces for ValueParsers, ValueFormatters and ValueValidators',
-		'license-name' => 'GPL-2.0+'
-	];
-}
